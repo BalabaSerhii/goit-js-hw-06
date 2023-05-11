0579e764df34be449a1e7128ba045e03b43cb428
BalabaSerhii <balabaserhii@gmail.com>
2023-05-11 20:50:06 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,28 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
+  
 ];
+
+// const ul = document.querySelector("#ingredients");
+
+// const items = ingredients.map((ingredient) => {
+//   const li = document.createElement("li");
+//   li.textContent = ingredient;
+//   li.classList.add("item");
+//   return li;
+// });
+
+// ul.append(...items);
+
+
+const ul = document.querySelector("#ingredients")
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ingredient;
+  ul.appendChild(li);
+});
+
+
