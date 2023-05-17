50e4296333414add0db5d068821b26c0aaf82ce0
BalabaSerhii <balabaserhii@gmail.com>
2023-05-17 16:52:56 +0200

    End(no)

--- js/task-04.js
+++ js/task-04.js
@@ -1,21 +1,13 @@
-// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
-// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
-// Обновляй интерфейс новым значением переменной counterValue.
-
-
 const minusNumber = document.querySelector('[data-action="decrement');
 const addNumber = document.querySelector('[data-action="increment');
 const result = document.querySelector("#value");
 
-
 let counterValue = 0;
-
 
 minusNumber.addEventListener("click", () => {
 	counterValue -= 1;
 	result.textContent = counterValue;
 })
-
 
 addNumber.addEventListener("click", () => {
 	counterValue += 1;
--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,5 @@
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка "Anonymous".
-
-
-
 const inputUser = document.querySelector("#name-input");
 let outputUser = document.querySelector("#name-output");
-
-
-
-
-
 
 inputUser.addEventListener('input', event => {
 	const result = event.target.value.trim() || 'Anonymous';
